5ddafab9b4f523616afa8321a0ffe8d30395ab15
LeoKao <leokao0726@gmail.com>
2017-04-24 20:44:57 +0900

    implement guess number
    finish guess number logic

--- GuessNumber/GuessNumber/ViewController.swift
+++ GuessNumber/GuessNumber/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    var answer = Int(arc4random_uniform(100)+1)
+    var maxNumber = 100
+    var minNumber = 1
+    var isOver = false
+    
+    @IBAction func guessButton(_ sender: UIButton) {
+        if isOver == false{     //continue game
+            print("Answer = \(answer)")
+            let guessNumber = Int(inputTextField.text!)
+            if guessNumber == nil {
+                messageLabel.text = String("Please enter a number")
+            }else{
+                if guessNumber! > maxNumber || guessNumber! < minNumber {
+                    messageLabel.text = String("Please input number between \(minNumber) ~ \(maxNumber)")
+                }else{
+                    if guessNumber! == answer{
+                        messageLabel.text = String("You are correct!! Press [Guess] to play again.")
+                        isOver = true
+                    }else{
+                        if guessNumber! < answer {
+                            minNumber = guessNumber!
+                        }else{
+                            maxNumber = guessNumber!
+                        }
+                        messageLabel.text = String("Number will between \(minNumber) ~ \(maxNumber)")
+                    }
+                }
+                inputTextField.text = ""
+            }
+        }else{  //set new game
+            maxNumber = 100
+            minNumber = 1
+            messageLabel.text = "Guess a number between \(minNumber) ~ \(maxNumber) :)"
+            isOver = false;
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        inputTextField.becomeFirstResponder()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
